336375668b22c66fa889d60c64a7eaf4d6f7d13c
Matti Tahvonen <matti@vaadin.com>
2018-09-04 11:51:37 +0300

    Initial project vie Spring Initializer

---
+++ src/main/java/org/vaadin/scp/SailingCoursePlannerApplication.java
@@ -0,0 +1,12 @@
+package org.vaadin.scp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SailingCoursePlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SailingCoursePlannerApplication.class, args);
+	}
+}
